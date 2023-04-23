1d3c8715b55528415467c43967ea72106ce20851
Catherine <rinny.cchi@gmail.com>
2023-04-24 04:03:52 +0800

    new assets, modify tweets, updated preload images

--- js/database/initProfiles.js
+++ js/database/initProfiles.js
@@ -10,43 +10,74 @@ var alhaitham   = new Profile(++j, "scribealhaitham", "Al Haitham", "alhaitham.j
 var childe      = new Profile(++j, "tartartartaglia", "Childe 🐳", "childe.jpeg", "childe.jpeg", "Stay still girlie ;) | pfp @tart____a", "Liyue");
 var cyno        = new Profile(++j, "gm_cyno", "Cyno", "cyno.jpeg", undefined, "Be careful of the religious group who has been going around to make people's life harder on purpose: The Diffi Cult.");
 var diluc       = new Profile(++j, "diluc_ragnvindr", "Diluc Ragnvindr", "diluc.jpeg", "diluc.jpeg", "Angel Share & Dawn Winery owner. Please stop messaging me, I will never give 'special discounts' to any mortal or gods alike.", "Mondstadt");
+var dottore     = new Profile(++j, "dodrugsresponsibly", "Segment #71", "dottore.jpeg", "dottore.jpeg", "", "Right behind you.");
+var fatui       = new Profile(++j, "Fatui_Official", "Fatui_Official", "Fatui_Official.jpeg", "Fatui_Official.jpeg", "Official account of the Fatui organization (real)", "Snezhnaya");
+var heizou      = new Profile(++j, "shikanoin_heizou", "Shikanoin Heizou", "heizou.jpeg", "heizou.jpeg", "Not my best looking picture but the developers said my original picture looks 'sus' and told me to change it... What does it even mean?? I'm the least suspicious person!", "Inazuma");
 var jean        = new Profile(++j, "jean_gunnhildr", "Jean Gunnhilder / KOF Acting Grand Master", "jean.jpeg", "jean.jpeg", "I am not active much on this app. Please report issues directly through a Knights of Favonius personnel, we will look into it as fast as possible.", "KOF Headquarters, Mondstadt");
 var kaveh       = new Profile(++j, "kavvvvveh", "🌻 Kaveh 🌷", "kaveh.jpeg", "kaveh.jpeg", "Kshahrewar graduate | Master architect | Wine connoiseour | Palace of Alcazarzaray my beloved magnum opus | Follow for aesthetic photos and beauty tips!", "Sumeru");
 var kaeya       = new Profile(++j, "kaeyalberich", "Oh woe is me", "kaeya.jpeg", "kaeya.jpeg", "If Jean is seeing this - I'm most definitely not skipping work!", "Fieldwork ;)");
 var lisa        = new Profile(++j, "librarianlady", "🌹", "lisa.jpeg", "lisa.jpeg", "Just your average librarian lady 😘<br>Return your books on time or else~", "Mondstadt");
 var nahida      = new Profile(++j, "dendro_archon", "🍀 Lesser Lord Kusanali 🌵", "nahida.jpeg", "nahida.jpeg", "Sumeru's Dendro Archon. It is nice to meet you!", "Sanctuary of Surasthana");
+var pantalone   = new Profile(++j, "regrator_fatui", "9th of Fatui Harbingers", "pantalone.jpeg", "pantalone.jpeg", "If you wish to talk business, please be a dear and contact the staffs in any Northland Banks branch near you.", "-");
 var tighnari    = new Profile(++j, "bigtallears", "Tighnari 🌳🏕", "tighnari.jpeg", "tighnari.jpeg", "Chief Officer of the Forest Rangers", "Avidya Forest");
-var wanderer    = new Profile(++j, "yourworstnightmare", "x", "wanderer.jpeg", undefined, "What a farce", undefined);
+var venti       = new Profile(++j, "ventithebard", "Mond's #1 Bard 🎵", "venti.jpeg", "venti.jpeg", "Ehe~", "The city of freedom");
+var wanderer    = new Profile(++j, "user7423984", "x", "wanderer.jpeg", undefined, "What a farce", undefined);
 var xiao        = new Profile(++j, "demonconqueror", "Xiao", "xiao.jpeg", "", undefined, "Liyue");
 var yunjin      = new Profile(++j, "yunjin_yunhanopera", "云堇 | Yun-Han Opera Troupe", "yunjin.jpeg", "yunjin.jpeg", "Director, playwright, and singer of the Yun-Han Opera Troupe. Follow to get notified of our next performance at Heyu Tea House!", "Liyue Harbor");
 var zhongli     = new Profile(++j, "notrexlapis", "Zhongli 🔸 Wangsheng Funeral Parlor Consultant", "zhongli.jpeg", "zhongli.jpeg", "Greetings. Wangsheng Funeral Parlor welcomes you anytime (Director Hu asked me to put this).", "Liyue Harbor", 45);
 
 // Initialize Profiles (People)
-var meirin      = new Profile(++j, "zhongrinofficial", "✾ mei/rin ✾", "meirin.png", "meirin.jpeg", "developer. zhongrin teahouse co-owner. wife of <span class='tag'>@notrexlapis</span> 💍💞", "Liyue");
-var albedoshusband = new Profile(++j, "albedoshusband", "misha <3", "albedoshusband.jpeg", undefined, "i'm totally straight ", "albedo's DMs");
+var meirin      = new Profile(++j, "zhongrinofficial", "✾ mei/rin ✾ <span class='vip'>👑</span>", "meirin.png", "meirin.jpeg", "developer. zhongrin teahouse co-owner. wife of <span class='tag'>@notrexlapis</span> 💍💞", "Liyue");
+var albedoshusband = new Profile(++j, "albedoshusband", "misha <3 <span class='vip'>🌟</span>", "albedoshusband.jpeg", undefined, "i'm totally straight ", "albedo's DMs");
 var StarySkies = new Profile(++j, "StarySkies", "Starz", "StarySkies.jpeg", undefined, "The moon sure is beautiful, huh? <br></br>(Beware I will go batshit insane one day L)", "TEYVAT I DONT KNOW SOBS");
-var BitesTheDust = new Profile(++j, "BitesTheDust", "Dust", "BitesTheDust.jpeg", undefined, "", "Mondstadt");
+var BitesTheDust = new Profile(++j, "BitesTheDust", "Dust <span class='vip'>🌟</span>", "BitesTheDust.jpeg", undefined, "", "Mondstadt");
 var heartstringed_boo = new Profile(++j, "heartstringed_boo", "boo", "heartstringed_boo.jpeg", undefined, "adventurer!!! i do fun things", "Inazuma");
-var soleilune = new Profile(++j, "soleilune", "Aly!", "soleilune.jpeg", undefined, "in my sleep deprived era", "in ur house");
+var soleilune = new Profile(++j, "soleilune", "Aly! <span class='vip'>🌟</span>", "soleilune.jpeg", undefined, "in my sleep deprived era", "in ur house");
 var Kalpalata2107 = new Profile(++j, "Kalpalata2107", "Lily ❀°。", undefined, undefined, "Just another sleep-deprived Akademiya student", "Sumeru City");
-var celestiebestie = new Profile(++j, "celestiebestie", "Celeste🩷🫧✨", "celestiebestie.jpeg", undefined, "don’t ask me how i got my pfp 🫶", "Liyue");
+var celestiebestie = new Profile(++j, "celestiebestie", "Celeste🩷🫧✨ <span class='vip'>🌟</span>", "celestiebestie.jpeg", undefined, "don’t ask me how i got my pfp 🫶", "Liyue");
 var taromao = new Profile(++j, "taromao", "taromao", "taromao.jpeg", undefined, "local mondstadt alchemist!", "mondstadt");
-var kaeyatiddiesgobrrr = new Profile(++j, "kaeyatiddiesgobrrr", "mika <3", "kaeyatiddiesgobrrr.jpeg", undefined, "*to the tune of the mcdonald's jingle accompanied by a broken kazoo sound* i'm losing it", "sumeru");
-var stonyballs = new Profile(++j, "stonyballs", "this is fine", "stonyballs.jpeg", undefined, "stuck in the chasm || who knew the chasm had free wifi", "The Chasmussy");
-var Crystalflygeo = new Profile(++j, "Crystalflygeo", "Crys", "Crystalflygeo.jpeg", undefined, "Proud Haravatat student. Dying bc of my thesis. Crystalfly gazer.", "Guili plains");
+var kaeyatiddiesgobrrr = new Profile(++j, "kaeyatiddiesgobrrr", "mika <3 <span class='vip'>🌟</span>", "kaeyatiddiesgobrrr.jpeg", undefined, "*to the tune of the mcdonald's jingle accompanied by a broken kazoo sound* i'm losing it", "sumeru");
+var stonyballs = new Profile(++j, "stonyballs", "this is fine <span class='vip'>🌟</span>", "stonyballs.jpeg", undefined, "stuck in the chasm || who knew the chasm had free wifi", "The Chasmussy");
+var Crystalflygeo = new Profile(++j, "Crystalflygeo", "Crys <span class='vip'>🌟</span>", "Crystalflygeo.jpeg", undefined, "Proud Haravatat student. Dying bc of my thesis. Crystalfly gazer.", "Guili plains");
 var dinasaurusrex = new Profile(++j, "dinasaurusrex", "dinaaaaa", "dinasaurusrex.jpeg", undefined, "the aranara have eyes everywhere 👀 | haitham's bestie ", "Puspa Cafe, Sumeru");
-var solxilynx = new Profile(++j, "solxilynx", "chxrry_noon 🌙 otr from a rat", "solxilynx.jpeg", undefined, "On the run from Pantalone and Wanderer. What's new?", "Anywhere and everywhere");
-var graybun = new Profile(++j, "graybun", "Gray", undefined, undefined, "", "Sumeru");
+var solxilynx = new Profile(++j, "solxilynx", "chxrry_noon 🌙 otr from a rat <span class='vip'>🌟</span>", "solxilynx.jpeg", undefined, "On the run from Pantalone and Wanderer. What's new?", "Anywhere and everywhere");
+var graybun = new Profile(++j, "graybun", "Gray <span class='vip'>🌟</span>", undefined, undefined, "", "Sumeru");
 var Explodingalchemist = new Profile(++j, "Explodingalchemist", "Luz ❄", undefined, undefined, "Ice ice babyyyy~<br></br>If you see Diona around alone don't freak out!! Don't tell me where you saw her all the time, I know where my sister is 😭😭", "The cat's tail backroom");
 var YingYue = new Profile(++j, "YingYue", "Ying Yue", "YingYue.jpeg", undefined, "", "Li Yue");
 var tsumimin13 = new Profile(++j, "tsumimin13", "tsu(ru)mi", undefined, undefined, "cryin and dyin (dramatically)", "the damned Akademiya");
-var lilyluvs = new Profile(++j, "lilyluvs", "lily", "lilyluvs.jpeg", undefined, "someone save me from uni </3", "mondstadt");
-var amurtacoco = new Profile(++j, "amurtacoco", "coco!♡", "amurtacoco.jpeg", undefined, "sexy, suffering, sleepy akademiya student . . zZzZ", "sumeru city");
+var lilyluvs = new Profile(++j, "lilyluvs", "lily <span class='vip'>🌟</span>", "lilyluvs.jpeg", undefined, "someone save me from uni </3", "mondstadt");
+var amurtacoco = new Profile(++j, "amurtacoco", "coco!♡ <span class='vip'>🌟</span>", "amurtacoco.jpeg", undefined, "sexy, suffering, sleepy akademiya student . . zZzZ", "sumeru city");
 var Reicrane = new Profile(++j, "Reicrane", "ReinDOM", "Reicrane.jpeg", undefined, "Coffee?", undefined);
-var wwflwr = new Profile(++j, "ww-flwr", "ang is currently going insane", "ww-flwr.jpeg", undefined, "call me a hydro vision user with the way I make things wet.", undefined);
-var xxx = new Profile(++j, "xxx", "name", "xxx.jpeg", undefined, "", "");
+var wwflwr = new Profile(++j, "ww-flwr", "ang is currently going insane <span class='vip'>🌟</span>", "ww-flwr.jpeg", undefined, "call me a hydro vision user with the way I make things wet.", undefined);
+var Silentmoth = new Profile(++j, "Silentmoth", "Moth <span class='vip'>🌟</span>", "Silentmoth.jpeg", undefined, "Screaming and then Silence", "Liyue");
+var CyanocittaCristata = new Profile(++j, "CyanocittaCristata", "Red(Blue)Jay", "CyanocittaCristata.jpeg", undefined, "Tova Schreier: Alchemist, Owner of Red Bird, Hobbyist Carpenter, Foodie.", "Mondstadt City, Mondstadt");
+var iamurworstnightmare = new Profile(++j, "iamurworstnightmare", "Aastha-licious <span class='vip'>🌟</span>", "iamurworstnightmare.jpeg", undefined, "i write books i guess.<br>and commit blasphemy.<br>writer stuff.", "everywhere bro");
+var nia_ama = new Profile(++j, "nia_ama", "Nia Amangkurat <span class='vip'>🌟</span>", "nia_ama.jpeg", undefined, "Songstress | Traveling Musician | Priestess | Currently in Sumeru | Status: Closed ⛔️ | Please send a reservation letter if you're interested in hiring me~♡ |", "Samudra Coast, Sumeru");
+var creamstewoah = new Profile(++j, "creamstewoah", "odelia ✰ <span class='vip'>🌟</span>", "creamstewoah.jpeg", undefined, "hi i bite people (with love)", "in a circus cage, most likely.");
+var imunlaukrknight = new Profile(++j, "imunlaukr.knight", "El", "imunlaukrknight.jpeg", undefined, "Weekly art groups @ Treasures street (approved by the Akademiya). Currently studying in Vahumana Darshan. Why does @FatuiOfficial keep following me", "Sumeru city");
+var froggy = new Profile(++j, "froggy", "Grand Frogmaster", "froggy.jpeg", undefined, "the day i make something edible is the day i confess my feelings", "Qingxu Pool");
+var Zatara = new Profile(++j, "Zatara", "ZAT🍒", "Zatara.jpeg", undefined, "Cherry is Life so is wine✨", "it’s complicated bud.");
+var dendrodame = new Profile(++j, "dendrodame", "Aedin is gathering grasses", "dendrodame.jpeg", undefined, "", "the ponds past Feiyun Slope");
+var blue_rain = new Profile(++j, "blue_rain", "name", "blue_rain.jpeg", undefined, "⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆hydrangea rain⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆<br>Hey, hey! It's your local florist!", "Fontaine");
+var moraxsbrowserhistory = new Profile(++j, "moraxsbrowserhistory", "rei <33 | comms open (7/10) | ArCon 20XX!!", "moraxsbrowserhistory.jpeg", undefined, "dm for commission info! | your provider of anything you can dream up <3 | if master ragnvindr from mond happens to see this i am free this friday. and all week", "liyue statue of seven's lap ");
+var cloudedxreality = new Profile(++j, "cloudedxreality", "Ichia♥︎", "cloudedxreality.jpeg", undefined, "My life is falling apart but that’s okay <3", "An unfamiliar place");
+// var xxx = new Profile(++j, "xxx", "name", "xxx.jpeg", undefined, "", "");
 
 Profiles.push(albedo, alhaitham, childe, cyno, diluc, jean, kaveh, kaeya, nahida, tighnari, wanderer, xiao, yunjin, zhongli);
 Profiles.push(meirin, albedoshusband, StarySkies, BitesTheDust, heartstringed_boo, soleilune, Kalpalata2107, 
     celestiebestie, taromao, kaeyatiddiesgobrrr, stonyballs, Crystalflygeo, dinasaurusrex, solxilynx,
-    graybun, Explodingalchemist, YingYue, tsumimin13, lilyluvs, amurtacoco, Reicrane, wwflwr, xxx);
+    graybun, Explodingalchemist, YingYue, tsumimin13, lilyluvs, amurtacoco, Reicrane, wwflwr, Silentmoth,
+    CyanocittaCristata, iamurworstnightmare, nia_ama, creamstewoah, imunlaukrknight, froggy, Zatara,
+    dendrodame, blue_rain, moraxsbrowserhistory, cloudedxreality);
+
+// TODO: loop through Profiles and preload images
+async function preload_image(im_url) {
+    let img = new Image();
+    img.src = im_url;
+    return "Ok";
+}
+Profiles.map(async(p) => {
+    await preload_image(p.getter("picture"));
+    await preload_image(p.getter("header"));
+    return p;
+});
--- js/database/initTweets.js
+++ js/database/initTweets.js
@@ -23,29 +23,49 @@
 //             "24m",
 //         ),
 //     ]
-// )) 
+// ))
+//
+// <span class='tag'>@tartartartaglia</span>
 //
 var Tweets = [], i = 0;
 
 Tweets.push(new Tweet(
     ++i,
     zhongli,
-    "Today's tale is as curious to listen to as usual.",
-    "30m",
+    "Today's tale is as riveting as usual.",
+    "2m",
     "images/tweet/zhongli1.jpeg",
     0,
     [
         new Tweet(
             ++i,
             meirin,
-            "❤️🥰💞💘💐🌼💗",
-            "12m",
+            "❤️🥰💞💘💐🌼💗 yes my lord whatever you say my lord",
+            "1m",
+        ),
+        new Tweet(
+            ++i,
+            Silentmoth,
+            "Why are there three cups on the table",
+            "2m",
+        ),
+        new Tweet(
+            ++i,
+            celestiebestie,
+            "Can I have one cup and mayhaps peruse the seat because it would be a waste to not drink tea while it's hot.",
+            "2m",
+        ),
+        new Tweet(
+            ++i,
+            iamurworstnightmare,
+            "Indeed. Very riveting. Yes. Very intriguing. *sips tea*",
+            "3m",
         ),
         new Tweet(
             ++i,
             yunjin,
-            "Yes, today's story is such an intriguing tale!",
-            "24m",
+            "Absolutely! Today's story is such an intriguing tale!",
+            "4m",
         ),
         
     ]
@@ -71,7 +91,6 @@ Tweets.push(new Tweet(
             "ARCHONS ABOVE GET A ROOM",
             "40m",
         ),
-        
     ]
 ))
 
@@ -182,6 +201,59 @@ Tweets.push(new Tweet(
 
 Tweets.push(new Tweet(
     ++i,
+    BitesTheDust,
+    "Just had to talk to the new Grand Sage of the Akademiya today...and WTF I might go back to uni??? Why did no one tell me they got such a hottie for the position. Excuse me but??<br>   ↳ <a href='https://twitter.com/_yuelight/status/1649763266380328963' target='_blank'>[source]</a>",
+    "1h",
+    "images/tweet/BitesTheDust.jpeg",
+    0,
+    [
+        new Tweet(
+            ++i,
+            alhaitham,
+            "I would advise against that notion. The Akademiya only chooses the best of the best, and only those who wish to seriously learn and master their crafts will prevail through the gruesome assignments and the mountains of research. Although I suppose if you're a capable individual like me, you might survive just fine.",
+            "1h",
+        ),
+        new Tweet(
+            ++i,
+            kaveh,
+            "<span class='tag'>@scribealhaitham</span> Excuse me what ☠️",
+            "1h",
+        ),
+        new Tweet(
+            ++i,
+            kaveh,
+            "I am so sorry for this uncouth ape's insufferable behavior, my lady <span class='tag'>@BitesTheDust</span>.",
+            "1h",
+        ),
+        new Tweet(
+            ++i,
+            meirin,
+            "holy shit he's smoking hot yeah dust for sure you go get that doctorate and tap dat badonkahonkers while you're at it",
+            "2h",
+        ),
+        new Tweet(
+            ++i,
+            solxilynx,
+            "Excuse me while I just examine the evidence of your claim 🔍<br>..... Hm. Yes, I suppose those are some nice mantits.",
+            "1h",
+        ),
+        new Tweet(
+            ++i,
+            alhaitham,
+            "What is wrong with you people.",
+            "2h",
+        ),
+        new Tweet(
+            ++i,
+            alhaitham,
+            "And what in the seven nations are 'badonkahonkers' and 'mantits'? What language are you all talking in?",
+            "1h",
+        ),
+    ]
+))
+
+Tweets.push(new Tweet(
+    ++i,
     tighnari,
     "Did you know that Shroomboars, according to researchers' studies, relies on the parasitic mushrooms perched on them for natural camouflage, which allows them to evade predators and survive in the jungles?",
     "1h",
@@ -191,7 +263,7 @@ Tweets.push(new Tweet(
         new Tweet(
             ++i,
             tighnari,
-            "However, this has also made it popular on the dinner table as naturally-occurring pre-made food. They're still quite dangerous, however. Please approach them with caution and always remember to hunt responsibly.",
+            "This fact has also made it popular on the dinner table as naturally-occurring pre-made food. They're still quite dangerous, however. Please approach them with caution and always remember to hunt responsibly.",
             "1h",
         ),
         new Tweet(
@@ -204,6 +276,12 @@ Tweets.push(new Tweet(
             ++i,
             tighnari,
             "I will bash you with a book.",
+            "1h",
+        ),
+        new Tweet(
+            ++i,
+            tighnari,
+            "Also did you just change your profile's description again??",
             "1h",
         ),
         new Tweet(
@@ -221,6 +299,29 @@ Tweets.push(new Tweet(
     ]
 ))
 
+// Tweets.push(new Tweet(
+//     ++i,
+//     xxx,
+//     "",
+//     "30m",
+//     null,
+//     0,
+//     [
+//         new Tweet(
+//             ++i,
+//             xxx,
+//             "",
+//             "12m",
+//         ),
+//         new Tweet(
+//             ++i,
+//             xxx,
+//             "",
+//             "24m",
+//         ),
+//     ]
+// ))
+
 Tweets.push(new Tweet(
     ++i,
     meirin,
@@ -232,13 +333,19 @@ Tweets.push(new Tweet(
         new Tweet(
             ++i,
             zhongli,
-            "I'm sorry dearest, I cannot seem to upload your photo. The world would have to stay deprived of your beauty for now.",
+            "I'm sorry, dearest - I cannot seem to upload your photographs on that day. The world would have to stay deprived of your beauty for now, but I implore you to teach me how to do such a task whenever you're able.",
+            "3h",
+        ),
+        new Tweet(
+            ++i,
+            meirin,
+            "🥰 oh you ☺️ ofc, anything for my hubby <3",
             "3h",
         ),
         new Tweet(
             ++i,
             childe,
-            "get a room you two",
+            "😃😃😃 for the love of tsaritsa, get a room you two",
             "3h",
         ),
         new Tweet(
@@ -249,8 +356,32 @@ Tweets.push(new Tweet(
         ),
         new Tweet(
             ++i,
+            zhongli,
+            "Dear, that is an inappropriate comment for such a public space.",
+            "3h",
+        ),
+        new Tweet(
+            ++i,
+            meirin,
+            "😘 but i mean it's true sooooo",
+            "3h",
+        ),
+        new Tweet(
+            ++i,
             childe,
-            "teucer give me strength",
+            "😔 teucer give me strength",
+            "3h",
+        ),
+        new Tweet(
+            ++i,
+            meirin,
+            "jokes on you we're like teucer's grandparents so",
+            "3h",
+        ),
+        new Tweet(
+            ++i,
+            childe,
+            ";(((",
             "3h",
         ),
     ]
